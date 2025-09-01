0d34dc8168ef69a587f516af44070832d16dd2e8
Matheus Brandao <matheusunai@gmail.com>
2025-09-01 01:03:23 -0300

    feat: listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-  <main>
-    <h1>E-Commerce</h1>
-  </main>  
+  <div className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'>
+      <div className='bg-white rounded-lg shadow-lg overflow-hidden'>Prod 1</div>
+      <div className='bg-white rounded-lg shadow-lg overflow-hidden'>Prod 1</div>
+      <div className='bg-white rounded-lg shadow-lg overflow-hidden'>Prod 1</div>
+      <div className='bg-white rounded-lg shadow-lg overflow-hidden'>Prod 1</div>
+    </div>
+  </div>  
   )
 }
